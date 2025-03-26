4b1eb96559cd378f6bac4288010a55a3d9280915
bru1f0rc3 <wint1841@inbox.ru>
2025-03-26 19:25:00 +0300

    .

--- BooksApi/Controllers/BookEventController/BookEventHistoryController.cs
+++ BooksApi/Controllers/BookEventController/BookEventHistoryController.cs
@@ -27,7 +27,44 @@ namespace BooksApi.Controllers.BookEventController
             try
             {
                 var events = await _eventService.GetUserBookEvents(accountId);
-                return Ok(new { events });
+                var history = new List<BookEventHistoryDTO>();
+
+                foreach (var evt in events)
+                {
+                    // Получаем информацию о книге
+                    var book = await _supabaseClient
+                        .From<Bookss>()
+                        .Match(new Dictionary<string, string> { { "id", evt.BookId.ToString() } })
+                        .Get();
+
+                    // Получаем информацию о пользователе
+                    var user = await _supabaseClient
+                        .From<Account>()
+                        .Match(new Dictionary<string, string> { { "id", evt.AccountId.ToString() } })
+                        .Get();
+
+                    if (book.Models.Count > 0 && user.Models.Count > 0)
+                    {
+                        // Получаем информацию об авторе
+                        var author = await _supabaseClient
+                            .From<Authors>()
+                            .Match(new Dictionary<string, string> { { "id", book.Models[0].Author.ToString() } })
+                            .Get();
+
+                        history.Add(new BookEventHistoryDTO
+                        {
+                            Id = evt.Id,
+                            BookTitle = book.Models[0].Title,
+                            BookAuthor = author.Models.Count > 0 ? author.Models[0].FullName : "Неизвестный автор",
+                            UserLogin = user.Models[0].Login,
+                            UserFullName = user.Models[0].FullName,
+                            EventType = evt.EventType,
+                            CreatedAt = evt.CreatedAt
+                        });
+                    }
+                }
+
+                return Ok(new { events = history });
             }
             catch (Exception ex)
             {
