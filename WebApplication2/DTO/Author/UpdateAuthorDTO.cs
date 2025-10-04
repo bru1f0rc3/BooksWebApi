57f8bbe2fb13300855cd6a73245a071cfa5cab17
Надир <144530385+bru1f0rc3@users.noreply.github.com>
2025-10-04 23:26:26 +0300

    Separate AuthorDTO classes for clarity.

---
+++ WebApplication2/DTO/Author/UpdateAuthorDTO.cs
@@ -0,0 +1,11 @@
+namespace WebApplication2.DTO.Author
+{
+    /// <summary>
+    /// DTO для обновления данных автора
+    /// Data Transfer Object for updating author information
+    /// </summary>
+    public class UpdateAuthorDTO
+    {
+        public string full_name { get; set; }
+    }
+}
