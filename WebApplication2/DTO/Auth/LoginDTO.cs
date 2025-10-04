ad361af52a3c3068ba0c4b16ae409066544b7c00
Надир <144530385+bru1f0rc3@users.noreply.github.com>
2025-10-04 23:24:01 +0300

    Fix nullable warnings in DTOs with required attributes and proper nullability

--- WebApplication2/DTO/Auth/LoginDTO.cs
+++ WebApplication2/DTO/Auth/LoginDTO.cs
@@ -1,16 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApplication2.DTO.Auth
 {
     public class LoginDTO
     {
-        public string Login { get; set; }
-        public string Password { get; set; }
+        [Required]
+        public required string Login { get; set; }
+        
+        [Required]
+        public required string Password { get; set; }
     }
-
+    
     public class TokenDTO 
     {
-        public string Token { get; set; }
-        public string Role { get; set; }
+        [Required]
+        public required string Token { get; set; }
+        
+        [Required]
+        public required string Role { get; set; }
+        
         public int UserId { get; set; }
-        public string FullName { get; set; }
+        
+        [Required]
+        public required string FullName { get; set; }
     }
-} 
+}
