e5544964d062eb16dd9a87bd1e36a5d67d0f29c3
bru1f0rc3 <yankeyanke793@gmail.com>
2025-11-01 12:45:00 +0300

    Add email and verification code fields to ChangePasswordDTO

---
+++ WebApplication2/DTO/Dashboard/ChangePasswordDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication2.DTO.Dashboard
+{
+    public class ChangePasswordDTO
+    {
+        public int id { get; set; }
+        public string old_password { get; set; }
+        public string new_password { get; set; }
+        public string email { get; set; }
+        public string code { get; set; }
+    }
+}
