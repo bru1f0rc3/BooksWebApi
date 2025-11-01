6d0e0c308632e1d5fa9881b37aba668b58001ae4
bru1f0rc3 <yankeyanke793@gmail.com>
2025-11-01 12:30:00 +0300

    Add verification code field to ChangeEmailUserDTO

---
+++ WebApplication2/DTO/Dashboard/ChangeEmailUserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication2.DTO.Dashboard
+{
+    public class ChangeEmailUserDTO
+    {
+        public int id { get; set; }
+        public string email { get; set; }
+        public string code { get; set; }
+    }
+}
